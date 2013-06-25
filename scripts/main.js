0e31535dc655083b0b15c26c65ebf117ef8c3515
oo7ph <Phillip@core-tech.com>
2013-06-25 08:43:31 -0600

    Added Code MIrror to replace textareas, built autoformatting for html

--- scripts/main.js
+++ scripts/main.js
@@ -2,55 +2,64 @@ require.config({
 	paths:{
 		"jquery"	: "vendor/jquery/jquery.min", 
 		"creatable"	: "vendor/creatable/index", 
-		"underscore": "vendor/underscore-amd/underscore-min", 
-	}
+		"codemirror": "vendor/codemirror/lib/codemirror", 
+		"codemirrorJavascriptMode": "vendor/codemirror/mode/javascript/javascript", 	
+		"codemirrorHTMLMode": "vendor/codemirror/mode/xml/xml", 	
+		"codemirrorHTMLMode": "vendor/codemirror/mode/xml/xml", 	
+	},
+	shim: {
+        'codemirrorJavascriptMode': ['codemirror'],
+        'codemirrorHTMLMode': ['codemirror'],        
+    }
 });
 
 require([
 	'jquery',
-	'creatable'
+	'creatable',
+	'codemirrorJavascriptMode',
+	'codemirrorHTMLMode',
+	'codemirror-formatting'
 
 ], function($){
-	var container = $('div.container');
-	var build = [[
-		['textarea.try-creatable'],
-		['textarea.try-html'],				
-	]];
+	function renderCodeMirror(){
+		var creatableCodeMirror = CodeMirror.fromTextArea(document.getElementById('creatableMarkup'), {
+			theme: 'blackboard',
+			height: "350px",
+		    autoMatchParens: true,
+			mode: {
+				name: 'javascript',
+				json: true,
+				typescript: true
+			}
+		});
+		var htmlCodeMirror = CodeMirror.fromTextArea(document.getElementById('htmlMarkup'), {
+			theme: 'blackboard',
+			height: "350px",
+		    autoMatchParens: true,
+			mode: {
+				name: 'xml',
+				htmlMode: true
+			}
+		});
+		
+		creatableCodeMirror.on('change', function(el){
+			try{
+				Creatable.setDocument(Creatable.emulatedDocument)
+				htmlCodeMirror.setValue(Creatable.create(eval(el.getValue())).toString());
+				htmlCodeMirror.autoFormatRange({line:0, ch:0}, {line: htmlCodeMirror.lineCount(), ch:htmlCodeMirror.getValue().length});
+			}
+			catch(error){
+				htmlCodeMirror.setValue('Enter Valid Creatable Array Syntax...');
+			}
+		});
+		
+		creatableCodeMirror.setValue("['.foo',[\n\t['p',[\n\t\t['a', { href:'google.com' }, 'google']\n\t]]\n]]");
+	};
 	
-	container.html(Creatable.create(build));
-	
-	container.on('keyup','.try-creatable' ,function(e){
-		var htmlTextbox = $('.try-html');
-		try{
-			Creatable.setDocument(Creatable.emulatedDocument)
-			htmlTextbox.val(Creatable.create(eval(e.target.value)).toString());
-		}
-		catch(e){
-			htmlTextbox.val('Enter Valid Creatable Array Syntax...');
-		}
-	});
-	
-	// Proper tab handling
-	$("textarea").keydown(function(e) {
-	    if(e.keyCode === 9) { // tab was pressed
-	        // get caret position/selection
-	        var start = this.selectionStart;
-	        var end = this.selectionEnd;
-	
-	        var $this = $(this);
-	        var value = $this.val();
-	
-	        // set textarea value to: text before caret + tab + text after caret
-	        $this.val(value.substring(0, start)
-	                    + "\t"
-	                    + value.substring(end));
-	
-	        // put caret at right position again (add one for the tab)
-	        this.selectionStart = this.selectionEnd = start + 1;
-	
-	        // prevent the focus lose
-	        e.preventDefault();
-	    }
-	});
+	$('div.container').html(Creatable.create([[
+		['textarea#creatableMarkup'],
+		['textarea#htmlMarkup'],				
+	]]));
+	renderCodeMirror();
 	
 });
